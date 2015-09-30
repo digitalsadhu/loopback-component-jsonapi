1fbd6ea4b45d0aed22dce02064cb0ba95381f4c4
Richard Walker <digitalsadhu@gmail.com>
2015-09-30 21:43:49 +0200

    Fixes for updating models, improve error logic

--- errors.js
+++ errors.js
@@ -2,17 +2,39 @@ module.exports = function (app, options) {
   var remotes = app.remotes();
 
   remotes.afterError('**', function (ctx, next) {
-    var errors = Object.keys(ctx.error.details.messages).map(function (key) {
-      return {
-        status: ctx.error.statusCode,
-        source: { pointer: 'data/attributes/' + key },
-        title: ctx.error.name,
-        code: ctx.error.details.codes[key][0],
-        detail: ctx.error.details.messages[key][0]
-      }
-    })
+    var errors = [];
+    var statusCode = 500;
 
-    ctx.res.status(ctx.error.statusCode).send({errors: errors});
+    //validation errors
+    if (ctx.error.details && ctx.error.details.messages) {
+      errors = Object.keys(ctx.error.details.messages).map(function (key) {
+        return {
+          status: ctx.error.statusCode,
+          source: { pointer: 'data/attributes/' + key },
+          title: ctx.error.name,
+          code: ctx.error.details.codes[key][0],
+          detail: ctx.error.details.messages[key][0]
+        }
+      });
+      statusCode = ctx.error.statusCode;
+    }
+
+    else if (ctx.error.message) {
+      statusCode = ctx.error.statusCode || ctx.error.status || 500;
+      errors.push({
+        status: statusCode,
+        detail: ctx.error.message
+      });
+    }
+
+    else {
+      errors.push({
+        status: 500,
+        detail: 'Internal server error'
+      });
+    }
+
+    ctx.res.status(statusCode).send({errors: errors});
     ctx.res.end();
   });
 }
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var serialize = require('./serialize');
 var deserialize = require('./deserialize');
 var removeRemoteMethods = require('./removeRemoteMethods');
 var create = require('./create');
+var update = require('./update');
 var errors = require('./errors');
 
 module.exports = function (app, options) {
@@ -14,5 +15,6 @@ module.exports = function (app, options) {
   serialize(app, options);
   deserialize(app, options);
   create(app, options);
+  update(app, options);
   errors(app, options);
 }
