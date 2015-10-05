170974bf5060573e08cdbb8cc7292d0d4058f890
Richard Walker <digitalsadhu@gmail.com>
2015-10-05 17:30:59 +0200

    Update to use body-parser middleware

--- headers.js
+++ headers.js
@@ -1,27 +1,7 @@
 "use strict";
 
+var bodyParser = require('body-parser');
+
 module.exports = function (app, options) {
-  app.use(function (req, res, next) {
-    if (req.header('Content-Type') && req.header('Content-Type') === 'application/vnd.api+json') {
-      req.rawBody = '';
-      req.setEncoding('utf8');
-
-      req.on('data', function(chunk) {
-        req.rawBody += chunk;
-      });
-
-      req.on('end', function() {
-        try {
-          req.body = JSON.parse(req.rawBody)
-        } catch (e) {
-          console.error('failed to parse raw request body')
-        }
-        console.log(req.body)
-        next();
-      });
-    } else {
-      console.log(req.body)
-      next();
-    }
-  });
+  app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 }
