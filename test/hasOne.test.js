e8a46f3a88e1095ea5ee2593087a0ab2db845bb2
Richard Walker <digitalsadhu@gmail.com>
2015-10-26 15:45:32 +0100

    Add hasOne tests for embedded relationship info
    In theory all someone needs to do is make these tests
    pass and we are good.

--- test/hasOne.test.js
+++ test/hasOne.test.js
@@ -144,5 +144,119 @@ describe('loopback json api hasOne relationships', function () {
           });
       });
     });
+
+    describe.skip('embedded relationship information in collections (GET /:collection)', function () {
+      it('should return author relationship link in relationships object', function (done) {
+        request(app).get('/posts')
+          .end(function (err, res) {
+            expect(err).to.equal(null);
+            expect(res.body.data[0].relationships).to.be.an('object');
+            expect(res.body.data[0].relationships.author).to.be.an('object');
+            expect(res.body.data[0].relationships.author.links).to.be.an('object');
+            expect(res.body.data[0].relationships.author.links.related).to.match(/posts\/1\/author/);
+            done();
+          });
+      });
+
+      it('should return included data as a compound document using key "included"', function (done) {
+        request(app).get('/posts?filter={"include":"author"}')
+          .end(function (err, res) {
+            expect(err).to.equal(null);
+            expect(res.body.data[0].relationships).to.be.an('object');
+            expect(res.body.data[0].relationships.author).to.be.an('object');
+            expect(res.body.data[0].relationships.author.data).to.deep.equal({
+              type: 'people',
+              id: '1'
+            });
+            expect(res.body.data[0].relationships.author.links).to.be.an('object');
+            expect(res.body.data[0].relationships.author.links.related).to.match(/posts\/1\/author/);
+            expect(res.body.included).to.be.an('array');
+            expect(res.body.included.length).to.equal(1);
+            expect(res.body.included[0]).to.have.all.keys('type', 'id', 'attributes', 'links');
+            expect(res.body.included[0].type).to.equal('people');
+            expect(res.body.included[0].id).to.equal('1');
+            done();
+          });
+      });
+
+      it('should return a 400 Bad Request error if a non existent relationship is specified.', function (done) {
+        request(app).get('/posts?filter={"include":"doesnotexist"}')
+          .expect(400)
+          .end(done);
+      });
+
+      it('should allow specifying `include` in the url to meet JSON API spec. eg. include=author', function (done) {
+        request(app).get('/posts?include=author')
+          .end(function (err, res) {
+            expect(err).to.equal(null);
+            expect(res.body.included).to.be.an('array');
+            expect(res.body.included.length).to.equal(1);
+            done();
+          });
+      });
+
+      it('should return a 400 Bad Request error if a non existent relationship is specified using JSON API syntax.', function (done) {
+        request(app).get('/posts?include=doesnotexist')
+          .expect(400)
+          .end(done);
+      });
+    });
+
+    describe.skip('embedded relationship information for individual resource GET /:collection/:id', function () {
+      it('should return author relationship link in relationships object', function (done) {
+        request(app).get('/posts/1')
+          .end(function (err, res) {
+            expect(err).to.equal(null);
+            expect(res.body.data.relationships).to.be.an('object');
+            expect(res.body.data.relationships.author).to.be.an('object');
+            expect(res.body.data.relationships.author.links).to.be.an('object');
+            expect(res.body.data.relationships.author.links.related).to.match(/posts\/1\/author/);
+            done();
+          });
+      });
+
+      it('should return included data as a compound document using key "included"', function (done) {
+        request(app).get('/posts/1?filter={"include":"author"}')
+          .end(function (err, res) {
+            expect(err).to.equal(null);
+            expect(res.body.data.relationships).to.be.an('object');
+            expect(res.body.data.relationships.author).to.be.an('object');
+            expect(res.body.data.relationships.author.data).to.deep.equal({
+              type: 'people',
+              id: '1'
+            });
+            expect(res.body.data.relationships.author.links).to.be.an('object');
+            expect(res.body.data.relationships.author.links.related).to.match(/posts\/1\/author/);
+            expect(res.body.included).to.be.an('array');
+            expect(res.body.included.length).to.equal(1);
+            expect(res.body.included[0]).to.have.all.keys('type', 'id', 'attributes', 'links');
+            expect(res.body.included[0].type).to.equal('people');
+            expect(res.body.included[0].id).to.equal('1');
+            done();
+          });
+      });
+
+      it('should return a 400 Bad Request error if a non existent relationship is specified.', function (done) {
+        request(app).get('/posts/1?filter={"include":"doesnotexist"}')
+          .expect(400)
+          .end(done);
+      });
+
+      it('should allow specifying `include` in the url to meet JSON API spec. eg. include=author', function (done) {
+        request(app).get('/posts/1?include=author')
+          .end(function (err, res) {
+            expect(err).to.equal(null);
+            expect(res.body.included).to.be.an('array');
+            expect(res.body.included.length).to.equal(1);
+            done();
+          });
+      });
+
+      it('should return a 400 Bad Request error if a non existent relationship is specified using JSON API syntax.', function (done) {
+        request(app).get('/posts/1?include=doesnotexist')
+          .expect(400)
+          .end(done);
+      });
+    });
   });
 });
